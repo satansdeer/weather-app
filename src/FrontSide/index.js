676beb3308a0d1b39b74431466e0eca6f072805c
Maksim Ivanov <satansdeer@gmail.com>
2018-08-19 20:34:53 +0200

    lesson-9

--- src/FrontSide/index.js
+++ src/FrontSide/index.js
@@ -5,18 +5,33 @@ import { getWeatherForLocation } from "../api";
 import cities from "../cities.json";
 
 class FrontSide extends Component {
+  state = { currentWeather: null };
+
   componentDidMount() {
-    getWeatherForLocation(cities[0]).then(console.log);
+    getWeatherForLocation(cities[0]).then(weather => {
+      this.setState({ currentWeather: weather.currently });
+    });
   }
 
   render() {
+    if (!this.state.currentWeather) {
+      return null;
+    }
+
+    const {
+      icon,
+      temperature,
+      apparentTemperature,
+      summary
+    } = this.state.currentWeather;
+
     return (
       <FrontSideView
         date={moment()}
-        icon="default"
-        temperature={19}
-        apparentTemperature={15}
-        summary="Sunny"
+        icon={icon}
+        temperature={temperature}
+        apparentTemperature={apparentTemperature}
+        summary={summary}
         currentCityName="Stockholm"
       />
     );
